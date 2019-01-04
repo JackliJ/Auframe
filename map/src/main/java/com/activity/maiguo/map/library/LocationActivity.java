358ec69a7e23890f6f005f2553b828bfe15fd719
zhanglu <903923374@qq.com>
2019-01-04 09:42:24 +0800

    1.暂存地图和颜色值

---
+++ map/src/main/java/com/activity/maiguo/map/library/LocationActivity.java
@@ -0,0 +1,13 @@
+package com.activity.maiguo.map.library;
+
+import com.maiguoer.component.http.app.BaseSwipeBackActivity;
+
+/**
+ * Created by laotie on 2019/1/3.
+ */
+
+public class LocationActivity extends BaseSwipeBackActivity {
+
+
+
+}
